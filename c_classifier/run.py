7e3fbec01404eff588f001d9404194ca43e2fbae
txete <72020520+txete@users.noreply.github.com>
2021-11-22 23:42:11 +0100

    EDA 1.0

--- c_classifier/run.py
+++ c_classifier/run.py
@@ -1,12 +1,6 @@
-import numpy as np
-
 from my_dataset import MyDataset
 import data_preprocessing as eda
 
-import tensorflow as tf
-
 
 train = MyDataset()
-train.read_data(datset_path='data/train/', verbose=False, shuffle=True)
-
-train.data[0] = eda.adaptative(train.data[0])
+train.read_data(datset_path='data/train/', verbose=True, shuffle=True)
